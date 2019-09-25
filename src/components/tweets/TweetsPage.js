04087df7c4579703ac63299cc82c3d892c442943
Elaiya Perumal S <CIPL0683@colanonline.local>
2019-09-25 18:17:14 +0530

    tweet page fix

--- src/components/tweets/TweetsPage.js
+++ src/components/tweets/TweetsPage.js
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { postTweet,getTweet } from '../../services/tweet/action';
+import { postTweet, getTweet } from '../../services/tweet/action';
 
-class TweetsPage extends Component{
+class TweetsPage extends Component {
     state = {
-        content:" "
+        content: " "
     }
     componentWillMount() {
         this.props.getTweet();
     }
-    onTweet = () => {
-
+    onTweet = (e) => {
+        const { content } = this.state;
+        e.preventDefault()
+        this.props.postTweet({ content })
     }
     handleChange = (e) => {
         this.setState({ [e.target.name]: e.target.value })
     }
-    render(){
+    render() {
         console.log(this.props)
-        return(
+        return (
             <section className="loginSection">
-                 <div className="loginBg">
-                     <div className="loginInner">
-                            <form>
-                                <h4 className="loginTitle">Tweeter</h4>
-                                <div className="form-group">
-                                    <textarea onChange={(e)=>this.handleChange(e)} type="text" name="content" className="form-control" />
-                                </div>
-                                <div className="form-group">
-                                    <button type="submit" className="btn" onClick={this.onTweet}>Tweet</button>
-                                </div>
-                            </form>   
+                <div className="loginBg">
+                    <div className="loginInner">
+                        <form>
+                            <h4 className="loginTitle">Tweeter</h4>
+                            <div className="form-group">
+                                <textarea onChange={(e) => this.handleChange(e)} type="text" name="content" className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <button type="submit" className="btn" onClick={(e) => this.onTweet(e)}>Tweet</button>
+                            </div>
+
+                            <ul>
+                                  {this.props.listTweetsData.map((data)=> <li>{data.content}</li>)}
+                            </ul>   
+                        </form>
                     </div>
                 </div>
             </section>
@@ -42,6 +48,6 @@ const mapStateToProps = state => {
         listTweetsData
     }
 }
-const mapDispatchToProps = { postTweet,getTweet }
+const mapDispatchToProps = { postTweet, getTweet }
 
 export default connect(mapStateToProps, mapDispatchToProps)(TweetsPage);
